c84fa3b10cee6696823c9a2022f3bffe4456605d
marijajolkic <mladenovicmilan499@gmail.com>
2025-10-25 13:51:22 +0200

    Add Material card and autocomplete to test component
    Enhanced the test-slide-toggle component with a Material card displaying sample content and a Material autocomplete element. Updated module imports in both home and test-slide-toggle components to support new Material features.

--- packages/client/src/app/features/home/pages/home/home.component.ts
+++ packages/client/src/app/features/home/pages/home/home.component.ts
@@ -2,10 +2,19 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../../layout';
 import { MatButton } from '@angular/material/button';
 import { TestSlideToggleComponent } from '../../../test-component/test-slide-toggle/test-slide-toggle.component';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage,
+  MatCardSubtitle,
+  MatCardTitle,
+} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [MatButton, TestSlideToggleComponent],
+  imports: [MatButton, TestSlideToggleComponent, MatCard, MatCardTitle, MatCardTitle],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
--- packages/client/src/app/features/test-component/test-slide-toggle/test-slide-toggle.component.ts
+++ packages/client/src/app/features/test-component/test-slide-toggle/test-slide-toggle.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatAutocomplete } from '@angular/material/autocomplete';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardImage, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-test-slide-toggle',
   imports: [
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatAutocomplete,
+    MatCard,
+    MatCardHeader,
+    MatCardSubtitle,
+    MatCardTitle,
+    MatCardImage,
+    MatCardContent,
+    MatCardActions,
+    MatButton,
   ],
   templateUrl: './test-slide-toggle.component.html',
-  styleUrl: './test-slide-toggle.component.scss'
+  styleUrl: './test-slide-toggle.component.scss',
 })
-export class TestSlideToggleComponent {
-
-}
+export class TestSlideToggleComponent {}
